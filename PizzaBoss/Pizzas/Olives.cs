ac481490fc7b4553f0cc25c8c008b2f63ea5cd05
accede10@mail.ru <accede10@mail.ru>
2019-02-26 21:40:40 +0200

    add Olives

---
+++ PizzaBoss/Pizzas/Olives.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaBoss.Pizzas
+{
+    class Olives : PizzaDecorator
+    {
+        public Olives(Pizza pizza) : base(pizza.name + ", с маслинами", pizza)
+        {
+
+        }
+
+        public override double Price()
+        {
+            return pizza.Price() + 6;
+        }
+    }
+}

a89f64c77720a7159c1a69f76c058d96b7896df2
accede10@mail.ru <accede10@mail.ru>
2019-02-26 21:39:42 +0200

    add Chilli

---
+++ PizzaBoss/Pizzas/Chilli.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaBoss.Pizzas
+{
+    class Chilli : PizzaDecorator
+    {
+        public Chilli(Pizza pizza) : base(pizza.name + ", с перцем чилли", pizza)
+        {
+
+        }
+
+        public override double Price()
+        {
+            return pizza.Price() + 4.5;
+        }
+    }
+}

71158562d144fdd22ff939742f4d27164d378732
accede10@mail.ru <accede10@mail.ru>
2019-02-26 21:21:54 +0200

    add Cheese

---
+++ PizzaBoss/Pizzas/Cheese.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaBoss.Pizzas
+{
+    class Cheese : PizzaDecorator
+    {
+        public Cheese(Pizza pizza) : base(pizza.name + ", с сыром", pizza)
+        {
+              
+        }
+
+        public override double Price()
+        {
+            return pizza.Price() + 7;
+        }
+    }
+}

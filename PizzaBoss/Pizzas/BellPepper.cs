577bf52fcbe35f57156c9ab9bfb00e4d5e86209f
accede10@mail.ru <accede10@mail.ru>
2019-02-26 21:38:42 +0200

    add BellPepper

---
+++ PizzaBoss/Pizzas/BellPepper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaBoss.Pizzas
+{
+    class BellPepper : PizzaDecorator
+    {
+        public BellPepper(Pizza pizza) : base(pizza.name + ", со сладким перцем", pizza)
+        {
+
+        }
+
+        public override double Price()
+        {
+        return pizza.Price() + 4.5;
+        }
+    }
+   
+}
